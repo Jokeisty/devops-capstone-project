0ab203fdf53aedff237f205960302534b2eb8099
jokeisty <jokeisty@gmail.com>
2026-07-20 13:04:03 -0400

    Configure Flask-Talisman

--- service/__init__.py
+++ service/__init__.py
@@ -14,7 +14,10 @@ from service.common import log_handlers
 app = Flask(__name__)
 app.config.from_object(config)
 
-Talisman(app)
+Talisman(
+    app,
+    force_https=False
+)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
